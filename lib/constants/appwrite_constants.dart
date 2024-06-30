124d6c24cb84b69c06fb0f049fcc0ea2f79dc980
hedmi-san <88062690+hedmi-san@users.noreply.github.com>
2024-06-30 18:24:00 +0100

    Initial commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '6681759c000ec015dace';
+  static const String projectId = '6681653c0024aa46f600';
+  static const String endPoint = 'http://localhost:80/v1';
+}
