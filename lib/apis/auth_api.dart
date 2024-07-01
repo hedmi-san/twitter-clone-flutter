aeeb1bb9ad41761809449baa4a45330eadced3a1
hedmi-san <88062690+hedmi-san@users.noreply.github.com>
2024-07-01 02:13:49 +0100

    configuring the user data fetch

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,8 +1,32 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as models;
+import 'package:fpdart/fpdart.dart';
 import '../core/core.dart';
 
+//want to sign up user, want to get user account --> Account
+//want to sign up user, want to get user account --> model.Account
 abstract class IAuthAPI {
-  FutureEither signUp({
+  FutureEither<models.User> signUp({
     required String email,
     required String password,
   });
+}
+
+class AuthAPI implements IAuthAPI {
+  late final Account _account;
+  AuthAPI({required Account account}) : _account = account;
+  @override
+  FutureEither<models.User> signUp(
+      {required String email, required String password}) async {
+    try {
+      final account = await _account.create(
+          userId: ID.unique(), email: email, password: password);
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+          Failure(e.message ?? 'Some unexpected error occured', stackTrace));
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -19,3 +19,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+//video 01:16:35
